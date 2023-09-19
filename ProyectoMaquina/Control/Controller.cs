079cc6925fe0dc2db7630dbe1f336454769b0bd8
adrianpizo <adrian.felipe.1994@gmail.com>
2023-09-19 01:09:56 -0500

    Primeras Modificaciones del repositorio

---
+++ ProyectoMaquina/Control/Controller.cs
@@ -0,0 +1,70 @@
+﻿using ProyectoMaquina.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoMaquina.Control
+{
+    public sealed class Controller
+    {
+        public static Controller _instance;
+        public List<IProduct> ListaProductos { get; set; }
+        private Controller() 
+        { 
+            ListaProductos = new List<IProduct>(); //nombre, precio, cantidad.
+
+            ListaProductos.Add(new Consumable("CocaCola", 3200, 5));
+            ListaProductos.Add(new Consumable("Snacks", 2500, 10));
+            ListaProductos.Add(new Consumable("Chocolocatina", 2800, 8));
+        }
+
+        public static Controller GetInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new Controller();
+            }
+            return _instance;
+        }
+
+        public string DisplayProductList()
+        {
+            string value = "";
+
+            foreach(IProduct product in ListaProductos)
+            {
+                value += product.DisplayProduct() + '\n';
+            }
+            return value;
+        }
+
+        public bool ProductExists(string product_name)
+        {
+            bool product_exists = false;
+
+            foreach(IProduct product in ListaProductos)
+            {
+                if(product.Name == product_name)
+                {
+                    product_exists = true;
+                }
+            }
+            return product_exists;
+        }
+        
+        public bool ProductHasInvenrory (string product_name)
+        {
+            bool has_inventory = false;
+            foreach(IProduct product in ListaProductos)
+            {
+                if(product.Name==product_name && product.Quantity > 0)
+                {
+                    has_inventory = true;
+                } 
+            }
+            return has_inventory;
+        }
+    }
+}
---
+++ ProyectoMaquina/View/Program.cs
@@ -0,0 +1,87 @@
+﻿using ProyectoMaquina.Control;
+using ProyectoMaquina.Model;
+using System;
+
+namespace ProyectoMaquina.View
+{
+    internal class View
+    {
+        static void Main(string[] args)
+        {
+            //Aqui empieza el Programa
+            Controller controller = Controller.GetInstance();
+
+            string texto_bienvenida = "Bienvenido a la Maquina Expendora...";
+            Console.WriteLine(texto_bienvenida);
+            string input_cliente = "";
+
+            while (true)
+            {
+                do
+                {
+                    Console.WriteLine("Escoja Tipo de Cliente: [C] o [P]");
+                    input_cliente = Console.ReadLine();
+
+                } while (input_cliente != "C" && input_cliente != "P"); //
+
+                Console.WriteLine("La Lista de Productos es: "); //TO-DO: Lista de Productos
+
+                 //FOR Para imprimir Productos con Controller
+                Console.WriteLine(controller.DisplayProductList());
+
+
+                if (input_cliente == "C")
+                {
+                    Console.WriteLine("Escoja un Producto de la Lista...");
+
+                    bool valid_product = false;
+                    do
+                    {
+                        string input_producto = Console.ReadLine();
+                        valid_product = controller.ProductExists(input_producto) && controller.ProductHasInvenrory(input_producto);
+
+                        if (valid_product == false)
+                        {
+                            Console.WriteLine("Escoja un Producto Valido: ");
+                        }
+                    } while (!valid_product);
+
+
+                    Console.WriteLine("Ingrese los Billetes para el pago del Producto: ");
+                    int suma_billete = 0;
+
+                    while (true)
+                    {
+
+                        Console.WriteLine("Ingrese el Billete...:");
+
+                        try
+                        {
+
+                            suma_billete += Convert.ToInt32(Console.ReadLine());
+
+                        }catch(FormatException e)
+                        {
+                            Console.WriteLine($"Por favor, ingrese un dato con valor numerico: {e.Message}");
+                        }
+
+
+                        Console.WriteLine("Para Dejar de Ingresar Billetes, escriba: [STOP] de lo contario presione ENTER");
+                        string input_cash = Console.ReadLine();
+                        if (input_cash == "STOP")
+                        {
+                            break;
+                        }
+                    }
+
+                    //Comparar precio producto vs suma_billete. Hacer Calculo
+
+                }
+                else if (input_cliente == "P")
+                {
+
+                }
+            }
+        }
+    }
+}
